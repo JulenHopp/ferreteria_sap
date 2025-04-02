c86ead91d67148a9fcb644cb0eb852b10ea9946b
ricardochapa203 <a01722755@tec.mx>
2025-04-02 15:35:55 -0600

    feat: primeros avances del front

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
-// src /main.jsx
+// src/main.jsx
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import AdminView from './views/AdminView.jsx'
+import AlmacenadorView from './views/AlmacenadorView.jsx'
+import CompradorView from './views/CompradorView.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminView />} />
+        <Route path="/almacenador" element={<AlmacenadorView />} />
+        <Route path="/comprador" element={<CompradorView />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
---
+++ src/views/CompradorView.jsx
@@ -0,0 +1,12 @@
+// src/views/CompradorView.jsx
+
+function CompradorView() {
+    return (
+      <div>
+        <h1>Bienvenido a la Vista del Comprador</h1>
+      </div>
+    )
+  }
+  
+  export default CompradorView
+  
