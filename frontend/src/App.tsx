d9c6be7dd79e7ebd3c522f292eaee0fe5ff0cd45
JulenHopp <112414883+JulenHopp@users.noreply.github.com>
2025-04-22 18:39:50 -0600

    Restarting frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from '@ui5/webcomponents-react';
+
+function App() {
+
+  return (
+    <>
+      <Button onClick={() => alert('Hello World!')}>Hello world!</Button>
+    </>
+  )
+}
+
+export default App
