3627724499ee5d5c16d81412b74e3f5f9515642c
JulenHopp <112414883+JulenHopp@users.noreply.github.com>
2025-05-14 13:17:51 -0600

    fixing frontend deployment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <BrowserRouter basement="/ferreteria_sap">
         <App />
       </BrowserRouter>
     </ThemeProvider>
